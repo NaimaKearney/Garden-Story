864c6f9476a607156bd7d8f0e8cb04f9717ea45b
Naima Kearney <naimakearney@gmail.com>
2022-09-22 15:33:59 -0400

    commit

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //This is a story of a garden. Good luck!
-document.write('Welcome')
+//Grow
+let sun = 3
+let water = 3
+
+const plantNeedsWater = (water) =>{
+    if (water <= 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const plantNeedsSun = (sun) =>{
+    if (sun <= 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+const veggies = ['tomato', 'pepper', 'kale'];
+veggies.forEach(veggieItem =>
+console.log("Your " + veggieItem + " is growing."));
+
+ 
+
